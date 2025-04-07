698a1eab42a306526d904f01d92774f58bc475bc
Kavin Sankar <123837409+ksankar15@users.noreply.github.com>
2025-04-07 13:14:37 -0400

    Backend implementation (#7)
    * Routes and models created
    
    * Moved service logic practice_test file
    
    * Minor correction to import statement
    
    * Implemented post and fake database functionality
    
    * Minor changes
    
    * Fixed file path import statement
    
    * All routes work with docs

---
+++ backend/models/academics/practice_test.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+from typing import Optional
+from fastapi import UploadFile
+
+class AIRequest(BaseModel):
+    """Requires user to input text to make a request, also allows them to upload a image or file"""
+
+    text: str
+    image: Optional[UploadFile] = None
+    file: Optional[UploadFile] = None
+
+class AIResponse(BaseModel):
+    """Outputs the test as a string"""
+    response_id: int
+    test: str
--- backend/services/health.py
+++ backend/services/health.py
@@ -9,18 +9,18 @@ would necessarily also become more complex to reflect the health of all subsyste
 In this context health does not refer to correctness as much as running, connected, and responsive.
 """
 
-from typing import Annotated
+#from typing import Annotated
 from fastapi import Depends
 from sqlalchemy import text
-from ..models.openai_test_response import OpenAITestResponse
+#from ..models.openai_test_response import OpenAITestResponse
 from ..database import Session, db_session
-from ..services.openai import OpenAIService
+#from ..services.openai import OpenAIService
 
 __authors__ = ["Kris Jordan"]
 __copyright__ = "Copyright 2023"
 __license__ = "MIT"
 
-
+'''
 class HealthService:
     _session: Session
     _openai_svc: OpenAIService
@@ -45,3 +45,16 @@ class HealthService:
         user_prompt = "Who is our most famous basketball player?"
         response_model = OpenAITestResponse
         return self._openai_svc.prompt(system_prompt, user_prompt, response_model)
+'''
+
+class HealthService:
+    _session: Session
+    
+    def __init__(self, session:Session = Depends(db_session)):
+        self._session = session
+    
+    def check(self):
+        stmt = text("SELECT 'OK', NOW()")
+        result = self._session.execute(stmt)
+        row = result.all()[0]
+        return str(f"{row[0]} @ {row[1]}")
