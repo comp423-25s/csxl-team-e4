3e6ac5bb0a989626b74504f7a10cd689ce8004e1
Siddhant Borkar <sborkar@unc.edu>
2025-04-06 23:09:23 -0400

    button routing

--- frontend/src/app/my-courses/course/practice/generate-test/generate-test.component.ts
+++ frontend/src/app/my-courses/course/practice/generate-test/generate-test.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
@@ -8,12 +8,18 @@ import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { SharedModule } from 'src/app/shared/shared.module';
-
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  Router,
+  RouterOutlet
+} from '@angular/router';
 
 @Component({
   selector: 'app-generate-test',
   standalone: true,
   imports: [
+    RouterOutlet,
     SharedModule,
     CommonModule,
     FormsModule,
@@ -28,10 +34,32 @@ import { SharedModule } from 'src/app/shared/shared.module';
   styleUrl: './generate-test.component.css'
 })
 export class GenerateTestComponent {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    effect(() => {
+      const url = this.router.url;
+      this.isResultPage.set(url.endsWith('/result'));
+    });
+
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isResultPage.set(this.router.url.endsWith('/result'));
+      }
+    });
+  }
+
   materials = [
     { timestamp: 'Thursday, 3/27 7:11PM', title: 'Study Guide Unit 2 Topic 3' },
     { timestamp: 'Thursday, 3/27 5:18PM', title: 'Study Guide Unit 2 Topic 4' },
     { timestamp: 'Thursday, 3/27 4:03PM', title: 'Study Guide Unit 2 Topic 1' }
   ];
+
   inputText = '';
+  readonly isResultPage = signal(false);
+
+  goToResultPage() {
+    this.router.navigate(['result'], { relativeTo: this.route });
+  }
 }
