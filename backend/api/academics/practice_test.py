a6156577e2cf10493c2056689a590f93ff92e5ac
Kavin Sankar <cary.sankars@gmail.com>
2025-04-06 20:21:20 -0400

    All routes work with docs

--- backend/api/academics/practice_test.py
+++ backend/api/academics/practice_test.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from ...models.academics.practice_test import AIResponse, AIRequest
-from backend.services.academics.practice_test import generate_test, retrieve_response
+from backend.services.academics.practice_test import make_test, get_AI_response, delete_AI_response
 from fastapi import HTTPException
 
 api = APIRouter(prefix="/api/academics/practice_test")
@@ -8,7 +8,7 @@ api = APIRouter(prefix="/api/academics/practice_test")
 @api.get("/retrieve_response/{response_id}", tags=["Academics"])
 def get_response(response_id: int):
     """Retrieve a response given a response id."""
-    response = retrieve_response(response_id)
+    response = get_AI_response(response_id)
     if response is None:
         raise HTTPException(status_code=404, detail="Response not found")
     return response
@@ -20,13 +20,13 @@ def generate_test(req: AIRequest):
     Takes in text and returns a dummy summary.
     """
     # Delegate to service (to be created)
-    result = generate_test(req.text, req.image, req.file)
-    return AIResponse(response_id=1, test = result)
+    id, result = make_test(req.text, req.image, req.file)
+    return AIResponse(response_id = id, test = result)
 
 @api.delete("/delete_response/{response_id}", tags=["Academics"])
 def delete_response(response_id: int):
     """Delete an AI response by ID."""
-    success = delete_response(response_id)
+    success = delete_AI_response(response_id)
     if not success:
         raise HTTPException(status_code=404, detail="Response not found")
     return f"Response {response_id} deleted successfully"
--- backend/services/academics/practice_test.py
+++ backend/services/academics/practice_test.py
@@ -3,19 +3,19 @@ from backend.models.academics.practice_test import AIResponse
 
 fake_responses_db = {}
 
-def retrieve_response(response_id: int) -> Optional[AIResponse]:
+def get_AI_response(response_id: int) -> Optional[AIResponse]:
     """Retrieve a specific AI response by ID."""
     return fake_responses_db.get(response_id)
 
-def delete_response(response_id: int) -> bool:
+def delete_AI_response(response_id: int) -> bool:
     """Delete an AI response by ID."""
     if response_id in fake_responses_db:
         del fake_responses_db[response_id]
         return True
     return False
 
-def generate_test(text, image, file) -> str:
-    new_id = max(fake_responses_db.keys()) + 1
+def make_test(text, image=None, file=None):
+    new_id = max(fake_responses_db.keys(), default=0) + 1
     test = "1. T/F is this fully implemented? Answer: False"
     fake_responses_db[new_id] = test
-    return test
+    return (new_id, test)
